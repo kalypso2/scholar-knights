79c666ec72b7f3d657fd3788e08cf5d779b2b65c
Ser-Orangabang <157593125+Ser-Orangabang@users.noreply.github.com>
2025-03-28 21:15:59 -0400

    SignupPage Skel

---
+++ scholar-knights/src/pages/SignupPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle.tsx';
+import { SignUp } from "/home/kelly/cards/frontend/src/components/SignUp"
+
+const SignUpPage = () => {
+
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+};
+
+export default SignUpPage;
