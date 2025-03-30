136cb39857242a610813bd58d47b62257f944f1b
kalypso2 <sam.ebaugh@gmail.com>
2025-03-29 21:05:03 -0400

    Updated port number

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.get("/", (req, res) => {
 });
 
 //  Start Server
-const PORT = 80;
+const PORT = 5001;
 
 app.listen(PORT, '0.0.0.0', () => console.log(` Server running on port ${PORT}`));
 
