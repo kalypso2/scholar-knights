f6b3f21d019f58018528846d6611113b1dc950db
imanimcq <125617565+imanimcq@users.noreply.github.com>
2025-03-30 14:39:53 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,2 +1,29 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+//  Define User Schema
+const UserSchema = new mongoose.Schema({
+  first_name: {
+  type:String,
+  required: true
+  },
+  last_name: {
+  type:String,
+  required: true
+  },
+  email: {
+  type:String,
+  required: true
+  },
+  username: {
+  type:String,
+  required: true
+  },
+  password:  {  // No hashing for now
+  type:String,
+  required: true
+  }, 
+  courses: [String],
+  groups: [mongoose.Schema.Types.Mixed],	
+});
+module.exports = user = mongoose.model("Users", UserSchema);
