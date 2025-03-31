90f652c9bc0f1ebd7baa87028c8678dd0353b319
imanimcq <125617565+imanimcq@users.noreply.github.com>
2025-03-31 13:27:06 -0400

    Update group.js

--- models/group.js
+++ models/group.js
@@ -7,7 +7,7 @@ const GroupSchema = new mongoose.Schema({
   capacity: Number,
   time: String, 
   date: Date,
-  members: [mongoose.Schema.Types.Mixed], //array of users
+  members: [{type:mongoose.Schema.Types.ObjectId, ref:'User'}], //array of users
   privacy: Boolean, //private is true public is false
 });
 module.exports = Group = mongoose.model('Group', GroupSchema);
